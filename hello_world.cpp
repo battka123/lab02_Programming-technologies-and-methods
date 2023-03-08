dec8d2b734262fae071143312eaa9b7349d71546
battka123 <i.suschin@yandex.ru>
2023-03-08 15:35:10 +0300

    Normal hello

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main(){
-  string name;
-  cout<<"Input your name: ";
-  cin>>name;
-  cout<<"Hello world from "<<name<<"!\n";
+  std::string name;
+  std::cout<<"Input your name: ";
+  std::cin>>name;
+  std::cout<<"Hello world from "<<name<<"!\n";
   return 0;
 }
