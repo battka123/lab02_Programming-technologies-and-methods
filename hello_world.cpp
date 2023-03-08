5e84462d65cdebb95a1703b40390c0e0f2ddce96
battka123 <i.suschin@yandex.ru>
2023-03-08 16:11:49 +0300

    New code style_2!

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,8 +5,8 @@ int
 main()
 {
   std::string name;
-  std::cout << "Input your name: ";
+  std::cout << "Input your name: "; \\new_name
   std::cin >> name;
-  std::cout << "Hello world from " << name << "!\n";
+  std::cout << "Hello world from " << name << "!\n"; \\Hello?
   return 0;
 }
